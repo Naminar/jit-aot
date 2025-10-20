32452d72e40e63886dcc40e7e2c93664b3549d53
Naminar <buravko.va@phystech.edu>
2025-10-20 20:14:20 +0300

    [upd] main file to use new IR builder

--- main.cc
+++ main.cc
@@ -5,24 +5,25 @@ int main() {
     IRBuilder builder;
 
     auto* entry = builder.createBasicBlock("entry");
-    builder.createInstruction("%x = alloca i32");
-    builder.createInstruction("store i32 10, i32* %x");
-    builder.createInstruction("%cmp = icmp sgt i32 10, 0");
-    builder.createBr("%cmp", "then", "else");
+    auto x = builder.createAlloca();
+    builder.createStore("42", x);
+    auto val = builder.createLoad(x);
+    auto cmp = builder.createICmp(ICmpInst::SGT, val, "0");
+    builder.createBr(cmp, "then", "else");
 
     auto* thenBB = builder.createBasicBlock("then");
-    builder.createInstruction("%a = add i32 10, 1");
+    auto a = builder.createAdd(val, "1");
     builder.createBr("merge");
 
     auto* elseBB = builder.createBasicBlock("else");
-    builder.createInstruction("%b = sub i32 10, 1");
+    auto b = builder.createSub(val, "1");
     builder.createBr("merge");
 
     auto* mergeBB = builder.createBasicBlock("merge");
     auto* phi = builder.createPHI();
-    phi->addIncoming("%a", "then");
-    phi->addIncoming("%b", "else");
-    builder.createRet("%phi");
+    phi->addIncoming(a, "then");
+    phi->addIncoming(b, "else");
+    builder.createRet(phi->name);
 
     builder.dump();
 
